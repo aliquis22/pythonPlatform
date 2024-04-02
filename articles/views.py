31dd9c06435645f0d9ac3a76285b00336b410e08
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-04-02 17:45:03 +0700

    Редактор статей готов

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
     return render(request, 'index.html', {'articles':articles})
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('home')
+    else:
+        form = ArticleForm()
+    data = {'form': form}
+    return render(request, 'articles/create.html', data)
 
 
