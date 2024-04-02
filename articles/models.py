6eaa337148385c0af12015fdecb50c6775691396
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-04-02 14:33:42 +0700

    Вывод статей готов

--- articles/models.py
+++ articles/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Article(models.Model):
+    title = models.CharField('Название', max_length=120)
+    announce = models.CharField('Анонс', max_length=200)
+    text = models.TextField('Статья')
+    image = models.ImageField(upload_to='static/images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
