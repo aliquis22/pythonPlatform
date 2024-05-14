7fdecb0d8fa4b9c205a8265ffc33ca78b1283950
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-05-14 13:31:59 +0700

    Добавил явное указание ссылок в меню

--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # URL-маршрут для главной страницы
+    path('', views.index, name='home'),  # URL-маршрут для главной страницы
     path('article/create', views.create, name='create'),
     # Другие URL-маршруты вашего приложения...
 ]
