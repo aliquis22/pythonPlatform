3bfb9eab236fcde69eebfa805ffcabfe2d04309a
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-05-17 21:48:54 +0700

    Добавил переход на страницу статьи

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,6 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='home'),  # URL-маршрут для главной страницы
     path('article/create', views.create, name='create'),
+    path('<int:id>/<slug:slug>', views.article_detail, name='article_detail')
     # Другие URL-маршруты вашего приложения...
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
 from .forms import ArticleForm
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -20,4 +21,7 @@ def create(request):
     data = {'form': form}
     return render(request, 'articles/create.html', data)
 
+def article_detail(request, id, slug):
+    article = get_object_or_404(Article, id=id, slug=slug)
 
+    return render(request, 'articles/article_detail.html', {'article': article})
