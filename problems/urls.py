1bf5b3652d31047c63800deb82989063848c2d7c
mds23 <mds23@tpu.ru>
2024-05-26 22:15:54 +0700

    PYT-114: Created problem page

--- problems/urls.py
+++ problems/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from .views import ProblemDetailView
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('<int:pk>/', ProblemDetailView.as_view(), name='problem-detail'),
 ]
--- problems/views.py
+++ problems/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Problem
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
     problems = Problem.objects.all()
 
     return render(request, 'problems/index.html', {'problems': problems})
+
+
+class ProblemDetailView(DetailView):
+    model = Problem
+    template_name = 'problems/detail.html'
+    context_object_name = 'problem'
