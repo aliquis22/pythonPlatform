39ccd51cf0c2cd3ff1f11be2a307a597b6d60ece
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-05-27 19:18:31 +0700

    Добавил кнопки удаления и редактирования комментария

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='home'),  # URL-маршрут для главной страницы
     path('article/create', views.create, name='create'),
-    path('<int:id>/<slug:slug>', views.article_detail, name='article_detail')
-    # Другие URL-маршруты вашего приложения...
+    path('<int:id>/<slug:slug>', views.article_detail, name='article_detail'),
+    path('comment/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
+from comments.models import Comment
 from .forms import ArticleForm
 from django.contrib.auth.decorators import login_required
 from comments.forms import CommentForm
@@ -41,4 +42,42 @@ def article_detail(request, id, slug):
     else:
         form = CommentForm()
     comments = article.comments.all()
+    # Обработка редактирования комментария
+    if 'edit_comment' in request.POST:
+        comment_id = request.POST.get('comment_id')
+        comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+        comment.text = request.POST.get('text')
+        comment.updated_date = timezone.now()
+        comment.save()
+        return redirect('articles:article_detail', id=article.id, slug=article.slug)
+
+    # Обработка удаления комментария
+    if 'delete_comment' in request.POST:
+        comment_id = request.POST.get('comment_id')
+        comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+        comment.delete()
+        return redirect('articles:article_detail', id=article.id, slug=article.slug)
     return render(request, 'articles/article_detail.html', {'article': article, 'form': form, 'comments': comments})
+
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.updated_date = timezone.now()
+            comment.save()
+            return redirect('articles:article_detail', id=comment.article.id, slug=comment.article.slug)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'articles/article_detail.html', {'form': form, 'comment': comment})
+
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    article_id = comment.article.id
+    article_slug = comment.article.slug
+    comment.delete()
+    return redirect('articles:article_detail', id=article_id, slug=article_slug)
