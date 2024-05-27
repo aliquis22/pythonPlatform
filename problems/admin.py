96febd6554b524e9ff12d5aa819d35a46d2f3a93
mds23 <mds23@tpu.ru>
2024-05-27 08:51:57 +0700

    Added problem list and detail page

--- problems/admin.py
+++ problems/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from problems.models import Problem, Test, ProblemTest, Code, ProblemCode
+from problems.models import Problem, Test, Code
 
 admin.site.register(Problem)
 admin.site.register(Test)
-admin.site.register(ProblemTest)
 admin.site.register(Code)
-admin.site.register(ProblemCode)
