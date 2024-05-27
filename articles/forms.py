c675c4394153fd9b73341e9e91958e2f4d5763e4
mds23 <mds23@tpu.ru>
2024-05-27 07:23:18 +0700

    Updated create article page

--- articles/forms.py
+++ articles/forms.py
@@ -6,8 +6,8 @@ class ArticleForm(ModelForm):
         model = Article
         fields = ['title', 'announce', 'text', 'image']
         widgets = {
-            "title": TextInput(attrs={'class':'form-control', 'placeholder': 'Введите название статьи'}),
-            "announce": Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите анонс статьи'}),
-            "text": Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите текст статьи', 'style': 'width: 100%; height: 200px;'}),
+            "title": TextInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5', 'placeholder': 'Введите название статьи'}),
+            "announce": Textarea(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5', 'placeholder': 'Введите анонс статьи'}),
+            "text": Textarea(attrs={'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500', 'placeholder': 'Введите текст статьи', 'style': 'width: 100%; height: 200px;'}),
             "image": FileInput(attrs={'class': 'form-control', 'placeholder': 'Выберите изображение'})
         }
